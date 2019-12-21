070a71bb74b0f95e27f59fad05c9f2e4782c3bf0
Ceshine Lee <shuanck@gmail.com>
2019-12-21 20:02:06 +0800

    Use Firestore; Update to the v12 API

--- bot.py
+++ bot.py
@@ -13,19 +13,25 @@ bot.
 import os
 import logging
 import datetime
+from pprint import pprint
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from retrying import retry
 import requests
+from google.cloud import firestore
+from google.oauth2 import service_account
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 AQI_TOKEN = os.environ["AQI_TOKEN"]
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keyfile.json"
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
+
+DB = firestore.Client()
 
 USEPA_PM25_2012 = [
     (0, 50, 0, 12),
@@ -45,11 +51,21 @@ def aqi_to_concentration(aqi):
     return round(aqi)
 
 
+def load_from_database():
+    # Then query for documents
+    subscription_ref = DB.collection(u'subscriptions')
+    subscriptions = {}
+    for doc in subscription_ref.stream():
+        subscriptions[doc.id] = doc.to_dict()
+    return subscriptions
+
+
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
-def start(bot, update):
+def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    update.message.reply_text(
+        f'Hi! {update.message.from_user.first_name}')
 
 
 def help(bot, update):
@@ -64,25 +80,31 @@ def echo(bot, update):
 
 def error(bot, update, error):
     """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
+    LOGGER.warning('Update "%s" caused error "%s"', update, error)
 
 
-@retry(stop_max_attempt_number=5, wait_fixed=5000)
-def get_reading(job):
+@retry(stop_max_attempt_number=3, wait_fixed=5000)
+def get_reading(station):
     aqi_data = requests.get(
         "http://api.waqi.info/feed/@{}/?token={}".format(
-            job.context["station"], AQI_TOKEN
+            station, AQI_TOKEN
         )
     ).json()
     if aqi_data["status"] != "ok":
-        logger.warning(aqi_data)
+        LOGGER.warning(aqi_data)
         raise RuntimeError("Failed to get the reading from the API server.")
     return aqi_data["data"]
 
 
-def broadcast_reading(bot, job):
-    logger.info("Triggered %s", str(job.context))
-    aqi_data = get_reading(job)
+def periodic_status_update(context: CallbackContext):
+    subscriptions = load_from_database()
+    for chat_id, data in subscriptions.items():
+        if "station" in data:
+            send_reading(context.bot, int(chat_id), data["station"])
+
+
+def send_reading(bot, chat_id, station: str):
+    aqi_data = get_reading(station)
     text = (
         "{} - {}\nAQI: \t*{}*\nPM 2.5 AQI:\t *{}*"
         "\nConcentration: *{}* ug/m3").format(
@@ -91,10 +113,10 @@ def broadcast_reading(bot, job):
         aqi_to_concentration(aqi_data["iaqi"]["pm25"]["v"])
     )
     bot.send_message(
-        job.context["chat_id"],
+        chat_id,
         text=text,
         parse_mode="markdown",
-        disable_notification=True
+        disable_notification=aqi_data["aqi"] <= 100
     )
 
 
@@ -106,61 +128,50 @@ def get_nearest_start():
             datetime.timedelta(hours=1))
 
 
-def set_notification(bot, update, args, job_queue, chat_data):
-    """Add a job to the queue."""
+def set_notification(update, context):
+    """Set a station to be tracked"""
     chat_id = update.message.chat_id
     try:
-        station = args[0]
-        context = {
-            "chat_id": chat_id,
+        station = context.args[0]
+        doc_ref = DB.collection('subscriptions').document(str(chat_id))
+        doc_ref.set({
+            "set_by": update.message.from_user.id,
             "station": station
-        }
-        # Remove existing
-        if "job" in chat_data:
-            chat_data["job"].schedule_removal()
-            del chat_data["job"]
-        # Add job to queue
-        job = job_queue.run_repeating(
-            broadcast_reading,
-            3600,
-            first=get_nearest_start(),
-            context=context
-        )
-        chat_data['job'] = job
+        })
         update.message.reply_text('Notification successfully set!')
-
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /set <station_id>')
 
 
-def unset(bot, update, chat_data):
-    """Remove the job if the user changed their mind."""
-    if 'job' not in chat_data:
-        update.message.reply_text("You have no active notifier")
-        return
-    job = chat_data["job"]
-    job.schedule_removal()
-    del chat_data["job"]
-    update.message.reply_text('Timer succefully unset!')
+def unset(update, context):
+    """Remove the tracking station."""
+    chat_id = update.message.chat_id
+    doc_ref = DB.collection('subscriptions').document(str(chat_id))
+    doc_ref.set({
+        "set_by": update.message.from_user.id
+    })
+    update.message.reply_text('Notification succefully unset!')
 
 
-def ondemand_broadcast(bot, update, chat_data):
-    if 'job' not in chat_data:
-        update.message.reply_text("You have no active notifier")
-        return
-    broadcast_reading(bot, chat_data["job"])
+def on_demand_broadcast(update, context):
+    chat_id = update.message.chat_id
+    data = DB.collection('subscriptions').document(str(chat_id)).get()
+    if data.exists is False or "station" not in data.to_dict():
+        update.message.reply_text('You need to /set a station first!')
+    else:
+        send_reading(context.bot, chat_id, data.get("station"))
 
 
-def find_station(bot, update, args):
+def find_station(update, context):
     """Find the nearest station"""
     try:
-        lat, lng = float(args[0]), float(args[1])
+        lat, lng = float(context.args[0]), float(context.args[1])
         data = requests.get(
             "http://api.waqi.info/feed/geo:{:.8f};{:.8f}/?token={}".format(
                 lat, lng, AQI_TOKEN
             )
         ).json()['data']
-        logger.info("Find station: %f %f", lat, lng, data)
+        LOGGER.info("Find station: %f %f %s", lat, lng, data)
         update.message.reply_text(
             "name: {} @ {};{} \n"
             "id: {}\n"
@@ -181,7 +192,8 @@ def find_station(bot, update, args):
 def main():
     """Start the bot."""
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater(BOT_TOKEN)
+    updater = Updater(BOT_TOKEN, use_context=True)
+    job_queue = updater.job_queue
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -195,7 +207,7 @@ def main():
                                   pass_chat_data=True))
     dp.add_handler(CommandHandler("find", find_station,
                                   pass_args=True))
-    dp.add_handler(CommandHandler("get", ondemand_broadcast,
+    dp.add_handler(CommandHandler("get", on_demand_broadcast,
                                   pass_chat_data=True))
     dp.add_handler(CommandHandler("unset", unset, pass_chat_data=True))
 
@@ -207,6 +219,10 @@ def main():
 
     # Start the Bot
     updater.start_polling()
+
+    job_queue.run_repeating(
+        periodic_status_update, interval=3600, first=get_nearest_start()
+    )
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
